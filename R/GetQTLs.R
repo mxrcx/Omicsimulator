863469d520bd28f8737f0dcd99b90f7271e1a5da
Marco <mxrcx.mail@web.de>
2019-06-28 14:35:24 +0200

    some approaches

--- R/GetQTLs.R
+++ R/GetQTLs.R
@@ -21,7 +21,47 @@ GetQTLs <- function(){
   trans_eQTL <- trans_eQTL[trans_eQTL_selection, ]
 
   # Save eQTL in maf format
+  # Minimum: Chromosome	Start_Position	Reference_Allele	Tumor_Seq_Allele2	Tumor_Sample_Barcode
 
+  # Annotate avinput format R data and file using ANNOVAR
+  annovar.dir <- "/opt/bin/annovar"
+  database.dir <- "/opt/bin/annovar/humandb"
+  chr = "chr1"
+  start = "123"
+  end = "123"
+  ref = "A"
+  alt = "C"
+  dat <- data.table(chr, start, end, ref, alt)
+  tmpfn <- tempfile()
+  write.table(dat, fn, row.names = FALSE, quote = FALSE, sep = "\t", col.names = FALSE)
+  x <- annotation(dat, "perl_annovar_refGene", annovar.dir = "/opt/bin/annovar",
+                  database.dir = database.dir)
+  x <- annotation(input.file = tmpfn, "perl_annovar_refGene", annovar.dir = "/opt/bin/annovar",
+                  database.dir = database.dir)
+
+
+  maf <- maftools::maf2maf(cis_eQTL)
+
+  print(maf)
+
+  sample.var = data.frame(chr = c('chr4', 'chr15'), start = c(55589774, 41961117),
+                          end = c(55589774, 41961117), ref_allele = c('A', 'TGGCTAA'), alt_allele = c('G', '-'))
+  write.table(sample.var, file='sampleVars.tsv', quote=FALSE, sep='\t', row.names = FALSE)
+  #write.table(sample.var, 'sampleVars.txt', sep='\t',quote = FALSE, row.names = FALSE)
+  var.maf <- oncotate(maflite = 'sampleVars.tsv', header = TRUE)
+
+  var.maf <- icgcSimpleMutationToMAF(icgc = 'sampleVars.tsv', addHugoSymbol = TRUE)
+
+  var.file = system.file("extdata", "sampleVars.tsv", package = "omicsimulator2.0")
+  print(var.file)
+  var.maf = oncotate(maflite = var.file, header = TRUE)
+
+
+  sample.var = data.frame(chromsome = c('chr4', 'chr15'), Start = c(55589774, 41961117),
+                          end = c(55589774, 41961117), ref = c('A', 'TGGCTAA'), alt = c('G', '-'),
+                          Tumor_Sample_Barcode = c('fake_1', 'fake2'))
+  write.table(sample.var, 'sampleVars.txt', sep='\t',quote = FALSE, row.names = FALSE)
+  var.maf <- oncotate(maflite = 'sampleVars.txt', header = TRUE)
 
   print(cis_eQTL)
 
